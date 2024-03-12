f1ca5cdfb565fe139d824df86b292007199ad394
promewizard <krisato93@gmail.com>
2024-03-12 09:36:03 -0700

    Next image tag, style up

--- next.config.js
+++ next.config.js
@@ -17,6 +17,16 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default config;
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,43 @@
+import type { User } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.imageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ctx}) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Author not found",
+        });
+      }
+      return {
+        ...post,
+        author,
+      };
+    });
   }),
 });
